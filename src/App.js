6ce11c67b1a3246c991020d0559717c3b970bae8
Paulo Rodrigues <pauloadesr@hotmail.com>
2020-08-10 13:23:58 -0300

    router config and styles global

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import './App.css';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      App React
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from './Footer.module.css';
+
+function Footer() {
+  return (
+    <div>
+      footer
+    </div>
+  )
+}
+
+export default Footer;
